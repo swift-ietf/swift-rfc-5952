b3e94c9ac9646dd767c54bac2805ed9222eb9f4f
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-03 07:34:41 +0200

    Apply mechanical lint fixes (swift-format, SwiftLint)

--- Package.swift
+++ Package.swift
@@ -9,8 +9,14 @@ extension String {
 extension Target.Dependency {
     static let rfc5952 = Self.target(name: .rfc5952)
     static let rfc4291 = Self.product(name: "RFC 4291", package: "swift-rfc-4291")
-    static let standards = Self.product(name: "Standard Library Extensions", package: "swift-standard-library-extensions")
-    static let incits41986 = Self.product(name: "ASCII Primitives", package: "swift-ascii-primitives")
+    static let standards = Self.product(
+        name: "Standard Library Extensions",
+        package: "swift-standard-library-extensions"
+    )
+    static let incits41986 = Self.product(
+        name: "ASCII Primitives",
+        package: "swift-ascii-primitives"
+    )
     static let rfc4648 = Self.product(name: "RFC 4648", package: "swift-rfc-4648")
 }
 
@@ -20,16 +26,22 @@ let package = Package(
         .macOS(.v26),
         .iOS(.v26),
         .tvOS(.v26),
-        .watchOS(.v26)
+        .watchOS(.v26),
     ],
     products: [
         .library(name: "RFC 5952", targets: ["RFC 5952"])
     ],
     dependencies: [
         .package(url: "https://github.com/swift-ietf/swift-rfc-4291.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-ietf/swift-rfc-4648.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-primitives.git",
+            branch: "main"
+        ),
+        .package(url: "https://github.com/swift-ietf/swift-rfc-4648.git", branch: "main"),
     ],
     targets: [
         .target(
@@ -39,7 +51,7 @@ let package = Package(
         .testTarget(
             name: "RFC 5952 Tests",
             dependencies: [
-                "RFC 5952",
+                "RFC 5952"
             ]
         ),
     ],
